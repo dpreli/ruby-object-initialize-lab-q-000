ef2c040cda4b71c23675ef16b8840847100a91d5
dpreli <dave.preli@gmail.com>
2016-04-01 09:57:28 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,23 +3,7 @@ class Dog
     @name = name
     @breed = breed
   end
-
-  def name=(name)
-    @name = name
-  end
-
-  def breed=(breed)
-    @breed = name
-  end
-
-  def name
-    @name
-  end
-
-  def breed
-    @breed
-  end
 end
 
-fido = Dog.new("Fido", "Pug")
+
 
